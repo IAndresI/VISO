ea0e0e7d4c27fec30065316546ff5ce1f415ebb0
IAndresI <andrei200277@gmail.com>
2020-09-08 22:39:15 +0300

    add plans

--- app/js/main.js
+++ app/js/main.js
@@ -27,4 +27,31 @@ document.addEventListener("DOMContentLoaded", function () {
     animate: true,
     tabToShow: 0
   });
+
+  let planButtons = document.querySelectorAll(".plan__button"),
+    planItems = document.querySelectorAll(".plan__item"),
+    planActiveLine = document.querySelector(".plan__active-line");
+
+
+  planButtons.forEach(element => {
+    element.addEventListener("click", function (e) {
+
+      planButtons.forEach(element => {
+        if (element == e.target) element.classList.add("plan__button--active");
+        else element.classList.remove("plan__button--active");
+      });
+
+      if (element.getAttribute("data-price") == "monthly") {
+        planActiveLine.style.transform = "translate(94%, 0)";
+        planItems.forEach(elements => {
+          elements.classList.add("plan__item--monthly");
+        });
+      } else {
+        planActiveLine.style.transform = "translate(0, 0)";
+        planItems.forEach(elements => {
+          elements.classList.remove("plan__item--monthly");
+        });
+      }
+    });
+  });
 });
